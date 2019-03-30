8a87f9f7717211f004367052047a3650a4d7f5af
FelipeCerero <lfcerero@misena.edu.co>
2019-03-30 16:07:07 -0500

    2019/03/30 16:05pm prueba de agregando con ionic-native, pero hay un incoveniente al agregar un market

--- src/app/vista-parque/vista-parque.page.ts
+++ src/app/vista-parque/vista-parque.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController,LoadingController } from "@ionic/angular";
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { ReturnStatement } from '@angular/compiler';
 declare var google;
 @Component({
   selector: 'app-vista-parque',
@@ -8,7 +9,12 @@ declare var google;
   styleUrls: ['./vista-parque.page.scss'],
 })
 export class VistaParquePage implements OnInit {
-  mapRef=null;
+  mapRef = null;
+  public arreglo: Array<{ lat: any, lng: any, title: string}> = [
+    {lat:4.5646909859967,lng:-74.1650178231759, title:'DESARROLLO ARBORIZADORA ALTA'}, 
+    {lat:4.5619427108137,lng:-74.1009777961402, title:'URBANIZACIÓN LAS GUACAMAYA'},
+    {lat:4.5751412157082,lng:-74.0813525154779, title:'SANTA ANA SUR ETAPA I'}  // local, base de datos dinamico
+  ];
   constructor(public navCtrl: NavController,private geolocation:Geolocation, public load:LoadingController) { }
 
   ngOnInit() {
@@ -23,30 +29,47 @@ export class VistaParquePage implements OnInit {
   }
 
   async loadMap(){
+    //Metodo o funcion que utilizamos para pintar icono de ubicacion, en donde se encuentre ubicado el dispositivo
     const loadging =  await this.load.create();
     loadging.present();
-    const rta = await this.geolocation.getCurrentPosition();
-    const mylatlng ={
-      lat: rta.coords.latitude,
-      lng: rta.coords.longitude
-    };
+    const mylatlng = await this.getLocation();
     const mapEle: HTMLElement = document.getElementById("map");
-    const map = new google.maps.Map(mapEle,{
+    this.mapRef = new google.maps.Map(mapEle,{
       center:mylatlng,
       zoom:12
     });
-    google.maps.event.addListenerOnce(map,'idle',()=>{
+    google.maps.event.addListenerOnce(this.mapRef,'idle',()=>{
         loadging.dismiss();
+        this.paintMarket(mylatlng.lat,mylatlng.lng);
     });
+    //
+    this.addMarket(); // llamar funcion del arreglo
+   
+  }
+
+  private paintMarket(lat:number,lng:number){
     const marker = new google.maps.Marker({
-      position:{
-        lat: mylatlng.lat,
-        lng: mylatlng.lng
-      },
+      position:{lat,lng},
       zoom:8,
-      map:map,
+      map:this.mapRef,
       title:'hello word'
     });
   }
+   private async getLocation (){
+    const rta = await this.geolocation.getCurrentPosition(); // agregar ubicacion actual
+    return {
+      lat: rta.coords.latitude,
+      lng: rta.coords.longitude
+    };  
+   }
+
+   private addMarket(){
+     for(let i=0;i<this.arreglo.length;i++){
+      this.paintMarket(this.arreglo[i].lat,this.arreglo[i].lng); // pintar los marcadores de las otras ubicaciones
+     }
+   }
+   currentLocate(event){
+     console.log(event);
+   }
 
 }
